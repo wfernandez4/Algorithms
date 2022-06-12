ea62c58e4bf5a6648f7d9563510c6e8d24b37537
William <wfernandez4@icloud.com>
2022-06-12 14:20:03 -0400

    Multiply two integers

--- recmult.py
+++ recmult.py
@@ -1,4 +1,4 @@
-def karatsuba(x,y):
+def recmult(x,y):
     # If the max length of either of the two string is odd, this will divide x and y appropriately.
     n = max(len(str(x)), len(str(y)))
     if n%2 != 0:
@@ -10,6 +10,6 @@ def karatsuba(x,y):
         b = int(x%(10**(n/2)))
         c = int(y/(10**(n/2)))
         d = int(y%(10**(n/2)))
-        return int((10**n)*(karatsuba(a,c)) + (10**(n/2))*(karatsuba(a,d)+karatsuba(b,c))+karatsuba(b,d))
+        return int((10**n)*(recmult(a,c)) + (10**(n/2))*(recmult(a,d)+recmult(b,c))+recmult(b,d))
         
-print(karatsuba(34443, 333))
+print(recmult(34443, 333))
